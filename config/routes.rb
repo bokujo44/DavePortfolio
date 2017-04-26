93e2dd7448399d17e924a82d59fb7bface042c4a
Dave <davidlarsen39@gmail.com>
2017-04-26 10:52:05 -0600

    error checking master"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  
   resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
@@ -8,12 +9,10 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
 
   resources :blogs do
- 	member do
- 		get :toggle_status
- 	end
- end
-
+    member do
+      get :toggle_status
+    end
+  end
+  
   root to: 'pages#home'
 end
-  
-

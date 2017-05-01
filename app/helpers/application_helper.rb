a6e95654806bbc8f12277c639877e715fbd45788
Dave <davidlarsen39@gmail.com>
2017-05-01 16:42:35 -0600

    refacoted alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
 
   def source_helper(layout_name)
     if session[:source]
-      greeting = "Thanks for checking me out #{session[:source]} and you are on the #{layout_name} layout"
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
@@ -59,5 +59,16 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
-end
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
 
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Dave Larsen", sticky: false)
+  end
+
+end

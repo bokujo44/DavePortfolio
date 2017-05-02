0430f07f738d153d27aca40152dc51abe2a521f9
Dave <davidlarsen39@gmail.com>
2017-05-02 13:47:15 -0600

    updated views

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -28,7 +28,7 @@ class PortfoliosController < ApplicationController
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+        format.html { redirect_to portfolios_path, notice: 'Item is now live.' }
       else
         format.html { render :new }
       end
